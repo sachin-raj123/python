cd58429d81518a681a48480228666ed45d064552
sachin-raj123 <sachinraj9986@gmail.com>
2025-12-24 22:13:13 +0530

    changes

---
+++ Lists & Tuples CH-03/Chapter 03.py
@@ -0,0 +1,65 @@
+                # Lists
+
+# marks = [45.7, 64.7, 78.9, 88.5, 92.3] 
+
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(marks[2])
+# print(marks[3])
+# print(marks[4])
+
+# student = ["sachin", 65, 64.7, True, "rahul"]
+# print(student)
+# print(student[0])
+# student[0] = "ankur"
+# print(student)
+
+                # List Slicing  #
+
+# marks = [54, 65, 83, 72, 91, 88, 77]
+
+# print(marks[1:5])  
+# print(marks[:4])
+# print(marks[3:])
+# print(marks[-4:-1])
+
+                #  List Methods  #
+
+# list = [5, 2, 9, 1, 7]
+# list.append(4)         # adds 4 at the end of list
+
+# list.sort()                # sorts the list in ascending order
+
+# list.reverse()   # reverse the list like  - 7, 1, 9, 2, 5
+
+# list.insert(2, 6)      # inserts 6 at index 2
+
+# list.remove(9)        # removes 9 from the list
+
+# list.pop()              # removes last element from the list
+
+# print(list)
+
+
+                # Tuples  in Python  #
+ 
+# tup = (2, 3, 4, 5, 6)
+# print(tup[0])
+# print(tup[1])
+# print(tup[2])
+  
+# tup = (1,)
+# print(tup)
+# print(type(tup))
+
+# tup = (1, 2, 3, 4)
+# print(tup.index(3))
+
+tup = (1, 2, 3, 4, 4, 1,3)
+print(tup.count(3))
+
+
+
+
---
+++ Lists & Tuples CH-03/practice.py
@@ -0,0 +1,55 @@
+                 # Practice Questions No. 01 
+
+# list = []
+# mov1 = input("Enter your favorite movie 1: ")
+# mov2 = input("Enter your favorite movie 2: ")
+# mov3 = input("Enter your favorite movie 3: ")
+
+# list.append(mov1)
+# list.append(mov2)
+# list.append(mov3)
+
+# print(list)
+
+                  # Practice Questions No. 02
+
+# list1 = [1, 2, 1]
+# list2 = [3, 4, 5]
+
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+
+# if(copy_list1 == list1):
+#     print("PALINDROME")
+# else:
+#     print("NOT PALINDROME")  
+
+
+# list1 = [1, 2, 1, 4]
+# list2 = [3, 4, 5, 3]
+
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+
+# if(copy_list1 == list1):
+#     print("PALINDROME")
+# else:
+#     print("NOT PALINDROME")    
+
+                 # Practice Questions No. 03
+
+# Count the number of students with "A" grade in the tuple 
+
+# grade = ("C", "D", "A", "A", "B", "B", "A")
+# print(grade.count("A"))
+
+                 # Practice Questions No. 04
+
+grade = ["A", "B", "C", "D", "B", "A", "C", "A"]
+grade.sort()
+print(grade)
+
+
+
+
+
